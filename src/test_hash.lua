5998e5efe5c58bc3bb3419b8173047179a847d5b
Meng Jie <inmouse@gmail.com>
2010-02-05 14:26:41 +0000

    add uuid generator.

--- src/test_hash.lua
+++ src/test_hash.lua
@@ -1,9 +1,19 @@
 #!/usr/bin/lua
 require 'hash'
 
-ret = hash.repr(hash.md5(arg[1]))
-print(string.format('md5("%s") = %s', arg[1], ret))
+--ret = hash.repr(hash.md5(arg[1]))
+--print(string.format('md5("%s") = %s', arg[1], ret))
+--
+--ret = hash.repr(hash.sha1(arg[1]))
+--print(string.format('sha1("%s") = %s', arg[1], ret))
+--
+data = string.rep("abcdefghij", 1024 * 1024 * 10)
 
-ret = hash.repr(hash.sha1(arg[1]))
-print(string.format('sha1("%s") = %s', arg[1], ret))
+--local file = io.open("tmp.data", "w")
+--file:write(data)
+--file:close()
+
+print(hash.repr(hash.sha1(data)))
+
+print(os.clock())
 
---
+++ src/test_uuid.lua
@@ -0,0 +1,30 @@
+require 'uuid'
+
+arg = {...}
+math.randomseed(os.time())
+local sum = 0
+local min = 999999999
+local times = 1
+
+for j = 1, times do
+	local mem = {}
+	local p
+	for i = 1, 10000000 do
+		--local ret = math.random(tonumber(arg[1]))
+		local ret = uuid.new()
+		--print(ret)
+		if mem[ret] then
+			--print('collision@'..i)
+			p = i
+			break
+		end
+		mem[ret] = true
+		p = i
+	end
+	if p < min then min = p end
+	sum = sum + p
+end
+
+print('average is '..sum/times)
+print('min is '..min)
+
